d7cef1de238be35c370ceb5b8f3e444883fa5260
Aashwin Mohan <aashwin@hotmail.co.uk>
2017-02-05 15:18:17 +0000

    More tests

--- tests/framework/modules/user_system.test.js
+++ tests/framework/modules/user_system.test.js
@@ -1,7 +1,10 @@
 var chai = require('chai');
 var expect = chai.expect;
+var sinon = require('sinon');
 var chaiAsPromised = require('chai-as-promised');
 var UserSystem = require('../../../framework/modules/user_system');
+var UserDAO = require('../../../framework/DAO/users.dao');
+var User = require('../../../framework/models/user');
 chai.use(chaiAsPromised);
 
 const GOOD_STRONG_PASSWORD = "Y@rdNB@!A$jj";
@@ -11,6 +14,15 @@ const SHORT_USERNAME = "aa";
 const SHORT_PASSWORD = "pass1";
 
 describe('Framework -> User System Tests', function () {
+    var sandbox;
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
     it('validateUser() should reject short username with 1 error message.', function () {
         const expectedReturn = {"errors": ["Username has to be between 3-50 characters"], "alreadyExists": false};
         var validationPromise = UserSystem.validateUser(SHORT_USERNAME, GOOD_STRONG_PASSWORD, GOOD_STRONG_PASSWORD);
@@ -25,6 +37,21 @@ describe('Framework -> User System Tests', function () {
     it('validateUser() should reject passwords that don\'t match with 1 error message.', function () {
         const expectedReturn = {"errors": ["Passwords must match!"], "alreadyExists": false};
         var validationPromise = UserSystem.validateUser(GOOD_USERNAME, GOOD_STRONG_PASSWORD, GOOD_STRONG_PASSWORD_2);
+        return expect(validationPromise).to.be.rejected.and.become(expectedReturn);
+    });
+    it('validateUser() should resolve when all validations passes', function () {
+        var findByUsernameStub = sandbox.stub(UserDAO, 'findByUsername');
+        findByUsernameStub.yields(null, new User(null));
+        var validationPromise = UserSystem.validateUser(GOOD_USERNAME, GOOD_STRONG_PASSWORD, GOOD_STRONG_PASSWORD);
+        return expect(validationPromise).to.be.fulfilled;
+    });
+    it('validateUser() should reject and alreadyExists should be false when username already exists', function () {
+
+        const expectedReturn = {"errors": ["Username already exists, try another!"], "alreadyExists": true};
+
+        var findByUsernameStub = sandbox.stub(UserDAO, 'findByUsername');
+        findByUsernameStub.yields(null, new User({"username": GOOD_USERNAME}));
+        var validationPromise = UserSystem.validateUser(GOOD_USERNAME, GOOD_STRONG_PASSWORD, GOOD_STRONG_PASSWORD);
         return expect(validationPromise).to.be.rejected.and.become(expectedReturn);
     });
 });
