52bc689b23a30938174c82a7fe996c80808c2e88
Aashwin Mohan <aashwin@hotmail.co.uk>
2017-02-07 00:40:30 +0000

    Making sure user is redirected properly

--- app/js/dashboard/header.controller.js
+++ app/js/dashboard/header.controller.js
@@ -6,7 +6,13 @@
         userService.getLoggedInUser().then(function (res) {
             if (res.success) {
                 $scope.user = res.user;
+            } else {
+                localStorage.removeItem("AUTH_TOKEN");
+                location.href = "/?unauthorised=true";
             }
+        }, function () {
+            localStorage.removeItem("AUTH_TOKEN");
+            location.href = "/?unauthorised=true";
         });
     }]);
 
