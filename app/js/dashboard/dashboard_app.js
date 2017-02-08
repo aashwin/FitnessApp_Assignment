7592140d959847ba2e2ea8ca43be40b3418afa31
Aashwin Mohan <aashwin@hotmail.co.uk>
2017-02-08 11:39:12 +0000

    Activities UI

--- app/js/dashboard/dashboard_app.js
+++ app/js/dashboard/dashboard_app.js
@@ -22,6 +22,72 @@
                 }
             };
         }]);
+    app.filter('numberText', function () {
+        return function (number, precision) {
+            if (isNaN(number)) {
+                return number;
+            }
+            precision = precision || 2;
+            var base = Math.floor(Math.log(Math.abs(number)) / Math.log(1000));
+            var suffix = 'kmb'[base - 1];
+            var prec = Math.pow(10, precision);
+            return suffix ? Math.round((number / Math.pow(1000, base) * prec)) / prec + suffix : '' + n;
+
+        };
+    });
+    app.directive('dashCard', function () {
+        return {
+
+            restrict: 'E', //E = element, A = attribute, C = class, M = comment
+            scope: {
+                //@ reads the attribute value, = provides two-way binding, & works with functions
+                title: '@',
+                value: "@",
+                format: "@",
+                units: "@",
+                type: "@"
+            },
+            // template: '<div class="dash_card" ng-class="class"> <div class="title">{{title}}</div><div class="value">{{displayVal}}</div></div>',
+            templateUrl: '/static_views/dashboard/dash_card.directive.html',
+            link: function (scope, elem, attrs) {
+                //Convert to K, M source adapted from: http://stackoverflow.com/a/10600491/7156780
+                scope.$watchGroup(['units', 'type', 'value'], function (newValues, oldValues, scope) {
+                    if (scope.type == 'timeValue' && scope.value) {
+                        var leftOver = scope.value;
+                        var hour = 0, minutes = 0, seconds = 0;
+                        if (scope.format.indexOf("HH") !== -1) {
+                            hour = Math.floor(leftOver / 3600);
+                            leftOver %= 3600;
+                        }
+                        if (scope.format.indexOf("MM") !== -1) {
+                            minutes = Math.floor(leftOver / 60);
+                            leftOver %= 60;
+                        }
+                        seconds = Math.round(leftOver);
+                        // if (scope.format.indexOf("HH") === -1 && scope.format.indexOf("MM") !== -1) {
+                        //     minutes = window.Math.round((scope.value) / 60);
+                        // }
+                        // if (scope.format.indexOf("MM") === -1 && scope.format.indexOf("SS") !== -1) {
+                        //     seconds = window.Math.round((scope.value / 60));
+                        // }
+
+                        scope.displayVal = scope.format.replace("HH", hour > 0 && hour < 10 ? "0" + hour : hour > 9 ? hour : "00");
+                        scope.displayVal = scope.displayVal.replace("MM", minutes > 0 && minutes < 10 ? "0" + minutes : minutes > 9 ? minutes : "00");
+                        scope.displayVal = scope.displayVal.replace("SS", seconds > 0 && seconds < 10 ? "0" + seconds : seconds > 9 ? seconds : "00");
+                    } else {
+                        scope.displayVal = scope.value ? scope.value : "--";
+
+                    }
+                    scope.displayUnits = scope.units;
+                    if (!scope.value) {
+                        scope.displayUnits = "";
+                    }
+                });
+            }
+
+
+        };
+    });
     app.config(['$routeProvider', '$locationProvider', '$httpProvider', 'momentPickerProvider', function ($routeProvider, $locationProvider, $httpProvider, momentPickerProvider) {
         momentPickerProvider.options({
             startView: 'month',
